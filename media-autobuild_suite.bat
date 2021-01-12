12c5bf6bb4d100d3a154bfdc631ff0efd53c236c
Philippe Noel <phil@tryfractal.com>
2021-01-12 16:30:38 -0500

    attempt build with specifying local path

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -1654,7 +1654,7 @@ if exist %instdir%\msys64\etc\profile.pacnew ^
     move /y %instdir%\msys64\etc\profile.pacnew %instdir%\msys64\etc\profile
 findstr /C:"profile2.local" %instdir%\msys64\etc\profile.d\Zab-suite.sh >nul 2>&1 || (
     echo.if [[ -z "$MSYSTEM" ^|^| "$MSYSTEM" = MINGW64 ]]; then
-    echo.   source /local64/etc/profile2.local
+    echo.   source /c/media-autobuild_suite/local64/etc/profile2.local
     echo.elif [[ -z "$MSYSTEM" ^|^| "$MSYSTEM" = MINGW32 ]]; then
     echo.   source /local32/etc/profile2.local
     echo.fi
